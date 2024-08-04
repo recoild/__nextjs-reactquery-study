8ddc21f80d0c1119d3f246fc3a715357c8479dbf
RamRam-V <cajhyung@gmail.com>
2024-08-04 23:35:38 +0900

    세션 정보 읽어오는 컴포넌트를 클라이언트 사이드로 변경

--- biz/components/profile-info.tsx
+++ biz/components/profile-info.tsx
@@ -1,15 +1,18 @@
+"use client";
 import { getServerSession } from "next-auth";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-import { FC } from "react";
+import { FC, use } from "react";
 import Login from "./login";
 import Logout from "./logout";
+import { useSession } from "next-auth/react";
 
 interface ProfileInfoProps {}
 
-const ProfileInfo: FC<ProfileInfoProps> = async ({}) => {
-    const session = await getServerSession(authOptions);
+const ProfileInfo: FC<ProfileInfoProps> = ({}) => {
+    // const session = await getServerSession(authOptions);
+    const { data: session } = useSession();
     if (session) {
         return (
             <div>
